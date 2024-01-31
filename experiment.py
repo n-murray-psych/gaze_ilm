b6805c7b92059e8ebbcadf44a4acee2422f93304
n-murray-psych <85261331+n-murray-psych@users.noreply.github.com>
2024-01-31 14:43:50 -0400

    Initial commit

---
+++ experiment.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Nicholas Murray"
+
+import klibs
+from klibs import P
+
+
+class gaze_ilm(klibs.Experiment):
+
+    def setup(self):
+        pass
+
+    def block(self):
+        pass
+
+    def trial_prep(self):
+        pass
+
+    def trial(self):
+
+        return {
+            "block_num": P.block_number,
+            "trial_num": P.trial_number
+        }
+
+    def trial_clean_up(self):
+        pass
+
+    def clean_up(self):
+        pass
