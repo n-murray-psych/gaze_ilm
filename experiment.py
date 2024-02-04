c89476106a912f75e5c874e8af1c4cd6066d35af
n-murray-psych <85261331+n-murray-psych@users.noreply.github.com>
2024-02-04 14:08:10 -0400

    Insertion of response rating scale from Austin

--- experiment.py
+++ experiment.py
@@ -9,13 +9,16 @@ from klibs.KLUserInterface import any_key, mouse_pos # So participants can press
 from klibs.KLGraphics import fill, blit, flip # To actually make drawn shapes appear on the screen
 from klibs.KLUtilities import deg_to_px # Convert stimulus sizes according to degrees of visual angle
 from klibs.KLResponseCollectors import KeyPressResponse # To take in key presses as a response to a trial
-from klibs.KLResponseListeners import KeypressListener # To record key press responses at the end of a trial
+from klibs.KLResponseListeners import KeypressListener, BaseResponseListener # To record key press responses at the end of a trial
 from klibs.KLConstants import TK_MS, RECT_BOUNDARY # to specify milliseconds as the unit of time to measure response times in, and the rectangle boundary for the line motion rating scale
 from klibs.KLEventInterface import TrialEventTicket as ET # to define the events of a trial according to stimulus timings
 from klibs.KLKeyMap import KeyMap # To map keys to responses and have them recorded in the database
 import sdl2 # To generate keyboard button names upon pressing them as a response
 from klibs.KLCommunication import message # To write messages on the screen to participants
 from klibs.KLBoundary import RectangleBoundary, BoundaryInspector # To create a boundary within which participants can rate line motion
+from klibs.KLEventQueue import pump, flush # Everything below recommended by Austin for drawing rating scale
+from klibs.KLUserInterface import mouse_pos
+from klibs.KLBoundary import RectangleBoundary
 
 # Defining some useful constants
 WHITE = (255, 255, 255)
@@ -122,6 +125,20 @@ class gaze_ilm(klibs.Experiment):
         ratingscalebasey2 = self.rating_scale_base_position[1]-(.5*ratingscalebaseheight)
         ratingscalebounds = RectangleBoundary(mouse_pos(), (ratingscalebasex1, ratingscalebasey1), (ratingscalebasex2, ratingscalebasey2))
         
+        # Response scale from Austin
+        scale_w = int(P.screen_x * 0.8)
+        scale_h = int(scale_w * 0.2)
+        scale_stroke = [int(scale_h * 0.1), BLACK, klibs.STROKE_INNER]
+        self.scale_loc = (P.screen_c[0], int(P.screen_y * 0.7))
+        
+        self.scale = kld.Rectangle(scale_w, scale_h, stroke=scale_stroke)
+        self.scale_mark = kld.Rectangle(int(scale_h * 0.1), scale_h, fill=BLACK)
+        self.scale_bounds = bounds_from_blit(self.scale, self.scale_loc)
+
+        self.scale_listener = ScaleListener(
+            self.scale_bounds, loop_callback=self.scale_callback
+        )
+
     #######################################################################################
         # FUNCTIONS DEFINING THE EXOGENOUS CUING TASK STIMULI
     #######################################################################################
@@ -449,10 +466,18 @@ class gaze_ilm(klibs.Experiment):
 
     def trial(self):
         self.detection_cuing_task()
-        flip()
-        self.rc.collect()
-        rt = self.rc.keypress_listener.response(False, True)
-        response = self.rc.keypress_listener.response(True, False)
+        
+        if self.task_requirement == "detection":
+            flip()
+            self.rc.collect()
+            rt = self.rc.keypress_listener.response(False, True)
+            response = self.rc.keypress_listener.response(True, False)
+        else:
+            fill()
+            blit(self.scale, 5, self.scale_loc)
+            flip()
+            resp, rt = self.scale_listener.collect()
+            print(resp, rt)
 
         return {
             "cue_type": self.cuing_task_type,
@@ -470,3 +495,131 @@ class gaze_ilm(klibs.Experiment):
 
     def clean_up(self):
         pass
+
+    def scale_callback(self):
+        mouse_x, mouse_y = mouse_pos()
+        scale_mid_y = self.scale_bounds.center[1]
+        fill()
+        blit(self.scale, 5, self.scale_loc)
+        if (mouse_x, mouse_y) in self.scale_bounds:
+            blit(self.scale_mark, 5, (mouse_x, scale_mid_y))
+        flip()
+
+
+REGISTRATION_MAP = {
+    1: (0, -1.0),
+    2: (-0.5, -1.0),
+    3: (-1.0, -1.0),
+    4: (0, -0.5),
+    5: (-0.5, -0.5),
+    6: (-1.0, -0.5),
+    7: (0, 0),
+    8: (-0.5, 0),
+    9: (-1.0, 0),
+}
+        
+def bounds_from_blit(rect, location, registration=5):
+    x_offset, y_offset = REGISTRATION_MAP[registration]
+    width, height = rect.dimensions
+    x1 = location[0] + int(width * x_offset)
+    y1 = location[1] + int(height * y_offset)
+    x2 = x1 + width
+    y2 = y1 + height
+    return RectangleBoundary('', (x1, y1), (x2, y2))
+
+
+class ScaleListener(BaseResponseListener):
+    """A convenience class for collecting continuous scale responses.
+
+    This class requires that the response scale has already been drawn to the
+    screen prior to response collection.
+
+    Args:
+        bounds (:obj:`~klibs.KLBoundary.RectangleBoundary`): A rectangle boundary
+            defining the size and location of the response scale.
+        start_pos (tuple, optional): The position to place the mouse cursor at the
+            start of the scale collection loop (in (x, y) pixel coordinates).
+            Defaults to the center of the screen if not specified.
+        timeout (float, optional): The maximum duration (in seconds) to wait for a
+            color response. Defaults to None (no timeout).
+        loop_callback (callable, optional): An optional function or method to be
+            called every time the collection loop checks for new input.
+
+    """
+    def __init__(self, bounds, start_pos=None, timeout=None, loop_callback=None):
+        super(ScaleListener, self).__init__(timeout, loop_callback)
+        self.default_response = (None, -1)
+        self._cursor_was_hidden = False
+        self._start_pos = start_pos if start_pos else P.screen_c
+        if not isinstance(bounds, RectangleBoundary):
+            raise TypeError("Scale bounds must be a RectangleBoundary object.")
+        self._bounds = bounds
+
+    def _timestamp(self):
+        return sdl2.SDL_GetTicks()
+        
+    def _get_scale_pos(self, cursor_pos):
+        if not pos in self._bounds:
+            return None
+        x1 = self._bounds.p1[0]
+        return (pos[0] - x1) / self._bounds.width
+
+    def init(self):
+        """Initializes the listener for response collection.
+
+        This method shows the mouse cursor and warps it to the start position.
+
+        Only needs to be called manually if using :meth:`listen` directly in a
+        custom collection loop.
+
+        """
+        # Start with cursor shown in start position
+        self._cursor_was_hidden = sdl2.ext.cursor_hidden()
+        sdl2.ext.show_cursor()
+        mouse_pos(position=self._start_pos)
+        # Clear any existing events in the queue and set the response start time
+        flush()
+        self._loop_start = self._timestamp()
+
+    def listen(self, q):
+        """Checks a queue of input events for continuous scale responses.
+
+        Along with :meth:`init` and :meth:`cleanup`, this method can be used to
+        create custom response collection loops in cases where :meth:`collect`
+        doesn't offer enough flexibility.
+
+        Args:
+            q (list): A list of input events to check for scale responses.
+
+        Returns:
+            tuple or None: A ``(response, rt)`` tuple if the scale has been
+            clicked, otherwise None.
+
+        """
+        for e in q:
+            if e.type == sdl2.SDL_MOUSEBUTTONUP:
+                # First, ensure mouse click was within the scale boundary
+                pos = (e.button.x * P.screen_scale_x, e.button.y * P.screen_scale_y)
+                if not pos in self._bounds:
+                    continue
+                # Next, calculate where the click was relative to the scale
+                x1 = self._bounds.p1[0]
+                resp = (pos[0] - x1) / self._bounds.width
+                rt = e.button.timestamp - self._loop_start
+                return (resp, rt)
+        return None
+
+    def cleanup(self):
+        """Performs any necessary cleanup after response collection.
+
+        For the scale listener, this method hides the mouse cursor if it wasn't
+        visible already when :meth:`init` was called, and resets the response
+        timer.
+
+        Only needs to be called manually if using :meth:`listen` directly in a
+        custom collection loop.
+
+        """
+        self._loop_start = None
+        if self._cursor_was_hidden:
+            sdl2.ext.hide_cursor()
